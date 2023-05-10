93f425372c35a3ba677b462ffbe65a1c533cface
V0lk7 <j.duval7@protonmail.com>
2023-05-10 19:44:13 +0200

    ajout verif dans print

--- philo/mandatory/utils/print.c
+++ philo/mandatory/utils/print.c
@@ -6,7 +6,7 @@
 /*   By: jduval <jduval@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/19 09:10:16 by jduval            #+#    #+#             */
-/*   Updated: 2023/05/10 08:57:55 by jduval           ###   ########.fr       */
+/*   Updated: 2023/05/10 19:40:52 by jduval           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,16 +18,19 @@ void	print_job(t_philo *philo, t_status status)
 	if (is_the_end(philo) == true)
 		return ;
 	pthread_mutex_lock(&philo->data->print);
-	philo->time.job = get_time(philo->time.zero);
-	if (status == FORK)
-		printf("%li %i has taken a fork\n", philo->time.job, philo->spot);
-	else if (status == EAT)
-		printf("%li %i is eating\n", philo->time.job, philo->spot);
-	else if (status == SLEEP)
-		printf("%li %i is sleeping\n", philo->time.job, philo->spot);
-	else if (status == THINK)
-		printf("%li %i is thinking\n", philo->time.job, philo->spot);
-	else if (status == DEAD)
-		printf("%li %i died\n", philo->time.job, philo->spot);
+	if (is_the_end(philo) == false)
+	{
+		philo->time.job = get_time(philo->time.zero);
+		if (status == FORK)
+			printf("%li %i has taken a fork\n", philo->time.job, philo->spot);
+		else if (status == EAT)
+			printf("%li %i is eating\n", philo->time.job, philo->spot);
+		else if (status == SLEEP)
+			printf("%li %i is sleeping\n", philo->time.job, philo->spot);
+		else if (status == THINK)
+			printf("%li %i is thinking\n", philo->time.job, philo->spot);
+		else if (status == DEAD)
+			printf("%li %i died\n", philo->time.job, philo->spot);
+	}
 	pthread_mutex_unlock(&philo->data->print);
 }
